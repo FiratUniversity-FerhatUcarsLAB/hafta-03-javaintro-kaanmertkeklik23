935b22ad6878d0ee42018fc50de93ce6075574a5
kaanmertkeklik23 <250541053@firat.edu.tr>
2025-10-23 17:54:18 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,9 @@
+public class DikdortgenHesap {
+    public static void main(String[] args) {
+        double en = 4.5, boy = 7.9;
+        double alan = en * boy;
+        double cevre = 2*(en+boy);
+        System.out.println("En: " + en + ", Boy: " + boy);
+        System.out.println("Alan: " + alan + ", Cevre: " + cevre);
+    }
+}
