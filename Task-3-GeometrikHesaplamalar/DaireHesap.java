39335803fddf085e9e64bedcd159ab4ae849a021
kaanmertkeklik23 <250541053@firat.edu.tr>
2025-10-23 17:54:49 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,10 @@
+public class DaireHesap {
+    public static void main(String[] args) {
+        double r = 5.5;
+        double alan = Math.PI * r * r;
+        double cevre = 2 * Math.PI * r;
+        System.out.println("Yaricap: " + r);
+        System.out.println("Alan: " + alan);
+        System.out.println("Cevre: " + cevre);
+    }
+}
