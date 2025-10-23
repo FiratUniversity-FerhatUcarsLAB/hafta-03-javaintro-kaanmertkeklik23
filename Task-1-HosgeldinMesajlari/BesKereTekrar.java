cb6e28e1eb935d6b63be32ceff871e852e30f173
kaanmertkeklik23 <250541053@firat.edu.tr>
2025-10-23 18:00:10 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class Hosgeldiniz {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Java'ya Hoşgeldiniz");
+        System.out.println("Java'ya Hoşgeldiniz");
+    }
+}
