47de54bc7245dbf90e25565677b11c8e3ef0c26a
kaanmertkeklik23 <250541053@firat.edu.tr>
2025-10-23 17:49:34 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,9 @@
+public class KaanAsciiArt {
+    public static void main(String[] args) {
+        System.out.println("K   K   AAAAA   AAAAA   N   N");
+        System.out.println("K  K    A   A   A   A   NN  N");
+        System.out.println("K K     AAAAA   AAAAA   N N N");
+        System.out.println("K  K    A   A   A   A   N  NN");
+        System.out.println("K   K   A   A   A   A   N   N");
+    }
+}
